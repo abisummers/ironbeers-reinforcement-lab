14a1f39417a290803281444179717ab453d6924d
abisummers <hello@abisummers.com>
2018-11-08 12:56:13 +0100

    added comments to the code

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 app.use(express.static(path.join(__dirname, "public")));
 
+//add registerPartials when you add partials.
+//must restart your terminal when you add this
+//if you change anything in app.js you need to restart the terminal
+//make sure the spelling is the same as the folder name
 hbs.registerPartials(__dirname + "/views/partials");
 
 app.get("/", (req, res, next) => {
@@ -17,10 +21,16 @@ app.get("/", (req, res, next) => {
 
 app.get("/beers", (req, res, next) => {
   punkAPI
+    //getBeers() comes from the punkAPI
     .getBeers()
+    //here, you need to use beers when referring to this later in beers.hbs
+    //the name isn't important, as long as you remember to use the same name
     .then(beers => {
+      //the console.log will appear in the terminal. It will show you what the object keys are which you'll need to use later
       console.log(beers);
-      res.render("beers", { beers });
+
+      //beers.hbs is where you want to use beers
+      res.render("beers.hbs", { beers });
     })
     .catch(error => {
       console.log(error);
@@ -29,8 +39,10 @@ app.get("/beers", (req, res, next) => {
 
 app.get("/random-beer", (req, res, next) => {
   punkAPI
+    //getRandom() comes from punkAPI.
     .getRandom()
     .then(beer => {
+      //Here, we only want to display one beer, so we pick index 0
       res.render("random-beer", { beer: beer[0] });
     })
     .catch(error => {
